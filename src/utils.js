77760a5b36223ecbeab22d6a2c0214bbddc213f6
Yurii Komarnytskyi <kv281214@gmail.com>
2022-05-26 14:18:40 +0300

    Finished logic of password creation that corresponds to user`s input on the UI part.

--- src/utils.js
+++ src/utils.js
@@ -1,37 +1,28 @@
 const piecesForPassword = [
     [...Array.from(Array(26)).map((val, ind) => val = String.fromCharCode(ind + 97))],
-    [...Array.from(Array(26)).map((val, ind) => val = String.fromCharCode(ind + 65))],
+    [...Array.from(Array(26)).map((val, ind) => val = String.fromCharCode(ind + 65))], // Big ones
     [0,1,2,3,4,5,6,7,8,9],
-    ['@','#','$','*','_'],
+    ['@','#','$','%','*','_'],
 ]
 
-
 const randIntRange = (s, e) => Math.floor(s + Math.random() * (e + 1 - s));
-const hasPresentChar = (str, kindOfChar) => {
 
+const pickRandArr = (arr) => {
+    const key = arr[randIntRange(0,3)];
+    return key[randIntRange(0, key.length-1)];
 }
-function genPass (length = 5, capLetters = false, integers = false, symb = false) {
-    let password = '';
-        for (let i = 0; i <= length; i++) {
-            if(i === 0) {
-                password += piecesForPassword[i][randIntRange(0, piecesForPassword[i].length-1)];
-                continue;
-            }
-            if(!arguments[i] && arguments[i] !== undefined) {
-                password += piecesForPassword[i][randIntRange(0, piecesForPassword[i].length-1)]
-            }
-            continue;
-        }
-        // передати у парольФіллер функцію ті самі аргументи що і в батьківської, слідкувати за довжиною наповнення
+
+function genPass (passLength = 5, capLetters = false, integers = false, symb = false) {
+    let password = ''
+    const passOptionsAccordToArgs = [
+        piecesForPassword[0],
+        (capLetters)? piecesForPassword[1] : piecesForPassword[0],
+        (integers)? piecesForPassword[2] : piecesForPassword[0],
+        (symb)? piecesForPassword[3] : piecesForPassword[0],
+    ]
+    for (let i = 0; i < passLength; i++) {
+        password += pickRandArr(passOptionsAccordToArgs);
+    }
     return password;
 }
-export {
-    genPass,
-    piecesForPassword,
-};
-
-
-// const intArr = [0,1,2,3,4,5,6,7,8,9];
-// const symbArr = ['@','#','$','*','_'];
-// const smallLetters = Array.from(Array(26)).map((val, ind) => val = String.fromCharCode(ind + 97));
-// const bigLetters = smallLetters.map(val => val.toUpperCase());
+export { genPass, }
