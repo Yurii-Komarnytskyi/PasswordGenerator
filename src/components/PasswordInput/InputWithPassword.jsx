f2f75cf2e773410224005c62d9a5b1d104605ccf
Yurii Komarnytskyi <kv281214@gmail.com>
2022-06-01 12:35:14 +0300

    deleted folder IndicationBar with its components, insted created indication bar in theInputWithPassword comp. removed text from it and set its height at 6px; Also additional header has been inserted between InputWithPassword and CheckBoxes

--- src/components/PasswordInput/InputWithPassword.jsx
+++ src/components/PasswordInput/InputWithPassword.jsx
@@ -1,21 +1,43 @@
-import React, { useRef } from 'react';
-import IndicatorOfDifficulty from '../IndicationBar/IndicatorOfDifficulty';
+import React, { useRef, useEffect } from 'react';
 import styles from './InputWithPassword.module.css';
 
-const InputWithPassword = ({password, wrapperForSetPass}) => {
+const InputWithPassword = ({ password, wrapperForSetPass, passwdLength }) => {
   const inpWithPass = useRef(null);
+  const passBar = useRef(null);
+
   const copyText = () => navigator.clipboard.writeText(inpWithPass.current.value);
+
+  const randerTheBar = (passwdLength) => {
+    if (passwdLength <= 8) {
+      passBar.current.className = styles.basicBar + ` ${styles.weakPass}`;
+    } else if (passwdLength > 8 && passwdLength <= 13) {
+      passBar.current.className = styles.basicBar + ` ${styles.mediumPass}`;
+    } else if(passwdLength >= 14) {
+      passBar.current.className = styles.basicBar + ` ${styles.strongPass}`;
+    }
+  }
+
+  useEffect(() => {
+    randerTheBar(passwdLength);
+  }, [passwdLength])
+
   return (
-    <div className={styles.inpWithPass}>
-        <input 
+    <React.Fragment>
+      <div className={styles.inpWithPass}>
+        <input
           readOnly
-          placeholder='Password appears here' 
-          value={password} 
+          placeholder='Password appears here'
+          value={password}
           ref={inpWithPass}
-        ></input> 
+        ></input>
         <button className={styles.bttnBeauty} onClick={wrapperForSetPass}>Create</button>
         <button className={styles.bttnBeauty} onClick={copyText}>Copy</button>
-    </div>
+      </div>
+      <div
+        className={styles.basicBar}
+        ref={passBar}
+      />  
+    </React.Fragment>
   )
 }
 
