895c2da0f54b000c91dcee16d354b3820fef820d
Yurii Komarnytskyi <kv281214@gmail.com>
2022-06-16 23:13:01 +0300

    Basic logic for operating credic card form, needs some mild improvements for the feilds which got to accept only certain amount of integers

--- src/Store/credCardFormSlice.js
+++ src/Store/credCardFormSlice.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import { createSlice } from "@reduxjs/toolkit";
+import { beautifyCardNumber } from "../utils";
 
-const credCardFormSlice = () => {
-  return (
-    <div>credCardFormSlice</div>
-  )
-}
+const credCardFormSlice = createSlice({
+  name: 'creditCardForm_Slice',
+  initialState : {
+    title: '',
+    cardholderName: '',
+    cardNumber: '',
+    cardBrand: '',
+    expirationMonth: '',
+    expirationYear: '',
+    cardSecurityCode: '',
+  },
+  reducers: {
+    gatherValueByStateKey(state, action) {
+      state[action.payload.dynamicKey] = action.payload.inputValue;
+    },
+    handleCardNumberInput(state, action){
+      if(isNaN(+action.payload)) return ;
+      state.cardNumber = action.payload;
 
-export default credCardFormSlice;
+      // if(state.cardNumber.length > 16) return ; NEED TO MAKE IT BEHAVE PRIDICTABLE
+      // state.cardNumber = beautifyCardNumber(state.cardNumber, action.payload);  NEED TO MAKE IT BEHAVE PRIDICTABLE
+    },
+    handleCardSecurityCodeInput(state, action){
+      if(isNaN(+action.payload)) return ;
+      if(state.cardSecurityCode.length > 2) return ; // NEED TO MAKE IT BEHAVE PRIDICTABLE
+      state.cardSecurityCode = action.payload;
+    },
+    handleExpirationYearInput(state, action) {
+      if(isNaN(+action.payload)) return ;
+      if(state.expirationYear.length > 3) return ; // NEED TO MAKE IT BEHAVE PRIDICTABLE
+      state.expirationYear = action.payload;
+
+    },
+  }
+
+})
+
+export const { 
+  gatherValueByStateKey, 
+  handleCardNumberInput,
+  handleCardSecurityCodeInput,
+  handleExpirationYearInput, } = credCardFormSlice.actions;
+export default credCardFormSlice.reducer;
