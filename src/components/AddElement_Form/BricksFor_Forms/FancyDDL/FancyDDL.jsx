c510df50dcadcbc3bbd315fc3ffdb467f34be106
Yurii Komarnytskyi <kv281214@gmail.com>
2022-06-16 23:16:27 +0300

    Done some minor changes to the files inside which will help to use lesser amount of reducers by using dynamic keys for updating a state

--- src/components/AddElement_Form/BricksFor_Forms/FancyDDL/FancyDDL.jsx
+++ src/components/AddElement_Form/BricksFor_Forms/FancyDDL/FancyDDL.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './FancyDDL.module.css';
-const FancyDDL = ({textForUpperSpan, arrOfOptions}) => {
+const FancyDDL = ({ textForUpperSpan, arrOfOptions, inputHandler, usersInput }) => {
   return (
     <div className={styles.wrapper}>
-        <span>{textForUpperSpan}</span>
-        <select  elect>
-          {arrOfOptions.map(opt => (<option>{opt}</option>))}
-        </select>
+      <span>{textForUpperSpan}</span>
+      <select 
+        onChange={(e) => inputHandler(e.target.value)}
+        value={usersInput}
+      >
+        {arrOfOptions.map(optn => (<option key={optn}> {optn} </option>))}
+      </select>
     </div>
   )
 }
--- src/components/AddElement_Form/BricksFor_Forms/FancyInput/FancyInputField.jsx
+++ src/components/AddElement_Form/BricksFor_Forms/FancyInput/FancyInputField.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from './FancyInputField.module.css';
-const FancyInputField = ({ textForUpperSpan, inputHandler, val }) => {
+
+const FancyInputField = ({ 
+  textForUpperSpan, 
+  inputHandler, 
+  usersInput, 
+  plHolder,
+  gotToHideInpText = false, }) => {
 
   return (
     <div className={styles.wrapper}>
       <span>{textForUpperSpan}</span>
       <input
-        type="text"
+        type={(gotToHideInpText)? 'password' : 'text'}
         onChange={(e) => inputHandler(e.target.value)}
-        value={val}
+        value={usersInput}
+        placeholder={(plHolder)? plHolder : ''}
       />
     </div>
   )
