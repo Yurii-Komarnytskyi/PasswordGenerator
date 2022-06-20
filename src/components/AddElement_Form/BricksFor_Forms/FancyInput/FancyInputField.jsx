b713ca7a9e286318d38d1c39f314c6bf667d0d82
Yurii Komarnytskyi <kv281214@gmail.com>
2022-06-20 14:02:31 +0300

    Added an aye button in order to hide and show input text at wil

--- src/components/AddElement_Form/BricksFor_Forms/FancyInput/FancyInputField.jsx
+++ src/components/AddElement_Form/BricksFor_Forms/FancyInput/FancyInputField.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from './FancyInputField.module.css';
 
 const FancyInputField = ({ 
@@ -8,16 +9,25 @@ const FancyInputField = ({
   plHolder,
   gotToHideInpText = false, }) => {
 
+  const [aye, setAye] = useState(gotToHideInpText);
+
   return (
     <div className={styles.wrapper}>
-      <span>{textForUpperSpan}</span>
+      <span className={styles.headerSpan}>{textForUpperSpan}</span>
       <input
-        type={(gotToHideInpText)? 'password' : 'text'}
+        className={styles.usersInput}
+        type={(aye)? 'password' : 'text'}
         onChange={(e) => inputHandler(e.target.value)}
         value={usersInput}
         placeholder={(plHolder)? plHolder : ''}
       />
-    </div>
+      {(gotToHideInpText)? (
+        <button 
+        onClick={()=> setAye(aye => !aye)}
+        className={styles.ayeButton}
+      ></button>
+      ) : null} 
+    </div> 
   )
 }
 
